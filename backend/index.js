d7501504c8536c3a186736196a481e85e610cb62
HarshilVora8160 <harshilvora8160@gmail.com>
2025-09-08 11:46:05 +0530

    i update my upload file data

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,23 @@ const express = require('express');
 const ConnectDb = require('./db');
 const router = require('./routes/SchoolRouter');
 const multer = require("multer")
+const upload = multer({ dest: "uploads/" });
 
 require('dotenv').config();
-const cors = require('cors')
-
-const upload = multer({dest :"uploads/"})
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4001
 
-
 const app = express();
-app.use(express.urlencoded({extended: false}))
+
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 const serverStart = async () => {
     try {
         await ConnectDb()
         app.get('/', (req, res, next) => {
-            res.json({message:"NodeJs"})
+            res.render({ message: "NodeJs" })
         })
 
         app.listen(PORT, () => {
@@ -28,10 +27,10 @@ const serverStart = async () => {
 
         app.use('/api', router)
 
-        app.post("/upload", upload.single("profileImage"), (req,res,next) => {
-            console.log("req.body-----------------",req.body);
-            console.log("req.file-----------------",req.file);
-            return res.redirect('/')
+        app.post("/upload", upload.single("profileImage"), (req, res, next) => {
+            console.log("req.body-----------------", req.body);
+            console.log("req.file-----------------", req.file);
+            return res.redirect('/');
         })
     } catch (error) {
         console.log(error)
